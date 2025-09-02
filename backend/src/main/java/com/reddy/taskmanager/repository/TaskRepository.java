d7409acee1fa4b8e8ab8778f09d6918e5c716a32
Reddy <nagendharreddy22@gmail.com>
2025-09-02 23:01:50 +0530

    📦 New : Added interfaces

---
+++ backend/src/main/java/com/reddy/taskmanager/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package com.reddy.taskmanager.repository;
+
+import com.reddy.taskmanager.entity.Task;
+import com.reddy.taskmanager.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    List<Task> findByOwner(User owner);
+    Optional<Task> findByIdAndOwner(Long id, User owner);
+}
