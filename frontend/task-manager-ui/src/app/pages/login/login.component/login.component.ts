9c197a36ca5f02847070d2ea43a92e0609a39821
Reddy <nagendharreddy22@gmail.com>
2025-09-20 23:36:49 +0530

    📦 NEW : add login component

---
+++ frontend/task-manager-ui/src/app/pages/login/login.component/login.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../core/auth.service';
+
+
+@Component({
+  selector: 'app-login.component',
+  standalone: true,
+  imports: [CommonModule, FormsModule, MatCardModule, MatButtonModule, MatInputModule, MatFormFieldModule, NgOptimizedImage],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  username = 'demo'; password = 'password';
+  constructor(private auth: AuthService, private router: Router) { }
+  submit() { this.auth.login(this.username, this.password).subscribe(() => this.router.navigateByUrl('/board')); }
+}
